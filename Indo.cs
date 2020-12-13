ba6a24ed1f1e3c21facaa09ab98bb011c7d1333a
Kauê Cardoso <54649009+Smutsia14@users.noreply.github.com>
2020-12-13 12:27:40 -0300

    Add files via upload

---
+++ Indo.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Indo : MonoBehaviour
+{
+    //Movimentação
+    public float Velocidade;
+    public Animator anim;
+    float InputX;
+    float InputZ;
+    Vector3 Direcao;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        InputX = Input.GetAxis("Horizontal");
+        InputZ = Input.GetAxis("Vertical");
+        Direcao = new Vector3(InputX,0,InputZ);
+        
+        if(InputX != 0 || InputZ != 0)
+        {
+            transform.Translate(0,0,Velocidade * Time.deltaTime);
+            anim.SetBool("Walk", true);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(Direcao), 5 * Time.deltaTime);
+        }
+        
+        if (InputX == 0 && InputZ == 0)
+        {
+            anim.SetBool("Walk", false);
+        }
+        
+        RunFaster();
+    }
+    void RunFaster()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            anim.SetBool("Run", true);
+        }
+        else
+        {
+            anim.SetBool("Run", false);
+        }
+    }
+    
+   
+}
---
+++ InimigoMov.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class InimigoMov : MonoBehaviour
+{
+    //Movimentação Enemy
+    public Transform playerTransform;
+    public NavMeshAgent nav;
+    float distanciaAtual;
+    
+    
+    //Ataque
+    SphereCollider areaAtk;
+    //bool atacando = false;
+    public float distanciaAtk = 2f;
+    float distanciaAtaque;
+    
+    public Animator anim;
+
+    void Start()
+    {
+        
+        anim = GetComponent<Animator>();
+        
+        
+        nav = GetComponent <NavMeshAgent>();
+        nav.SetDestination (playerTransform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        distanciaAtual = Vector3.Distance (transform.position, playerTransform.transform.position);
+        nav.SetDestination (playerTransform.position);
+        
+        
+        if (distanciaAtual <= distanciaAtk )
+        {
+        anim.SetBool("atk", true);
+        }
+        else{
+        anim.SetBool("atk", false);
+        }
+        
+    }
+}
